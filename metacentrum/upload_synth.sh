77d815a73db8362ebac0ba94436b0060fe74c741
bulirma <uni@bulirma.xyz>
2026-06-07 15:53:09 +0200

    adjust metacentrum scripts

--- metacentrum/upload_synth.sh
+++ metacentrum/upload_synth.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 rsync -urv --files-from=- ./ metacentrum:byzkit/ <<EOF
-metacentrum/prepare_job.sh
-metacentrum/job_synth.pbs
 common
 dataset
-byzkit.py
 byztex/template_standalone.tex
+EOF
+
+rsync -urv --no-relative --files-from=- ./ metacentrum:byzkit/ <<EOF
+metacentrum/prepare_job.sh
+metacentrum/job_synth.pbs
 notrack/public/dsp1200.zip
 EOF
